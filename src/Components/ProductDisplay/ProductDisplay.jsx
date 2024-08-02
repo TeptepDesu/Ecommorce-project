5f55626f463f1b55bd3faf1b266aec5297b55aae
TeptepDesu <caballerojosephirah@gmail.com>
2024-08-02 18:40:09 +0800

    Display in Items update

--- src/Components/ProductDisplay/ProductDisplay.jsx
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -21,13 +21,41 @@ const ProductDisplay = (props) => {
             </div>
             <div className="productdisplay-right">
                 <h1>{product.name}</h1>
-                <div className="productdisplay-right-star">
+                <div className="productdisplay-right-stars">
                     {[...Array(4)].map((_, index) => (
                         <img key={index} src={star_icon} alt={`Star ${index + 1}`} />
                     ))}
                     <img src={star_dull_icon} alt="Star" />
                     <p>(122)</p>
                 </div>
+                <div className="productdisplay-right-prices">
+                    <div className="productdisplay-right-price-old">
+                        ${product.old_price}
+                    </div>
+                    <div className="productdisplay-right-price-new">
+                        ${product.new_price}
+                    </div>
+                </div>
+                <div className="productdisplay-right-description">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi et nibh rutrum, sagittis turpis a, vulputate enim. Nullam consequat posuere massa.
+                </div>
+                <div className="productdisplay-right-size">
+                    <h1>Select Size</h1>
+                    <div className="productdisplay-right-sizes">
+                        <div>S</div>
+                        <div>M</div>
+                        <div>L</div>
+                        <div>XL</div>
+                        <div>XLL</div>
+                    </div>
+                </div>
+                <button>ADD TO CART</button>
+                    <p className="productdisplay-right-category">
+                        <span>Category: </span> Women , T-Shirt, Crop Top
+                    </p>
+                    <p className="productdisplay-right-category">
+                        <span>Tags: </span> Modern , Latest
+                    </p>
             </div>
         </div>
     );
